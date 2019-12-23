eef66bc3c587df9381b8929b760391f58a1763c2
Micael Jarniac <Micael@Jarniac.com>
2019-12-23 01:23:56 -0300

    Proof of concept script

---
+++ SevenSegTools.py
@@ -0,0 +1,37 @@
+numbers     = open("fonts/numbers", "r")
+alphabet    = open("fonts/alphabet", "r")
+
+for i in numbers:
+    b = 0b0
+    print(i, end = '')
+    c = i[4:11]
+    if c[0] == '1':
+        print(" #### ")
+    else:
+        print(" .... ")
+    for x in range(3):
+        if c[5] == '1':
+            print("#    ", end = '')
+        else:
+            print(".    ", end = '')
+        if c[1] == '1':
+            print("#")
+        else:
+            print(".")
+    if c[6] == '1':
+        print(" #### ")
+    else:
+        print(" .... ")
+    for x in range(3):
+        if c[4] == '1':
+            print("#    ", end = '')
+        else:
+            print(".    ", end = '')
+        if c[2] == '1':
+            print("#")
+        else:
+            print(".")
+    if c[3] == '1':
+        print(" #### ")
+    else:
+        print(" .... ")
